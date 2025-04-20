16b7ecaceb3d0b9a1729be42974a1d38081207c7
saksham1919 <saksham19@tamu.edu>
2025-04-20 13:21:48 -0500

    Adds fire loader component

---
+++ frontend/src/components/FireLoader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FireLoader = () => (
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    paddingTop: '2rem',
+  }}>
+    <img src="fire-loader.gif" alt="Loading fire data..." style={{ width: '120px', height: '120px' }} />
+    <p style={{ color: '#ff5722', fontSize: '1.2rem', marginTop: '1rem' }}>Loading fire data...</p>
+  </div>
+);
+
+export default FireLoader;
+
